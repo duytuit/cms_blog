5a00b53b4ea31a5611e1f7bbd90daaf669b91526
Duy Tu <duytu89@gmail.com>
2023-08-23 21:45:36 +0700

    dev

--- app/Views/admin/includes/_header.php
+++ app/Views/admin/includes/_header.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title><?= htmlspecialchars($title); ?> - <?= trans("admin"); ?>&nbsp;<?= htmlspecialchars($settings->site_title); ?></title>
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <meta name="verify-admitad" content="7d346d9505" />
     <?php if (empty($generalSettings->favicon_path)): ?>
         <link rel="shortcut icon" type="image/png" href="<?= base_url('assets/img/favicon.png'); ?>"/>
     <?php else: ?>
